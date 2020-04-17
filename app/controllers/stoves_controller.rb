95e3de8b206bac8a73bd13fbd51723f5babd988a
Mathieu Lague <mathieu.lague@laposte.net>
2020-04-17 11:35:55 +0200

    create show stove

--- app/controllers/stoves_controller.rb
+++ app/controllers/stoves_controller.rb
@@ -1,5 +1,16 @@
 class StovesController < ApplicationController
+  # before_action find_stove, only: :show
   def index
     @stoves = Stove.all
+  end
+
+  def show
+    @stove = Stove.find(params[:id])
+  end
+
+  private
+
+  def find_stove
+    @stove = Stove.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :stoves, only: [ :index ]
+  resources :stoves, only: [:index]
+  resources :stoves, only: [:show], as: :stove
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
