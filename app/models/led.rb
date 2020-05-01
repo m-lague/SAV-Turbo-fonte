8817e71811dad97f29244cb30ac58e5bc50847fc
Mathieu Lague <mathieu.lague@laposte.net>
2020-05-01 15:56:01 +0200

    create a form for displayed diagnostic

--- app/models/led.rb
+++ app/models/led.rb
@@ -3,4 +3,8 @@ class Led < ApplicationRecord
   has_many :alarms, through: :alarms_leds
 
   enum status: [:on, :off, :blink]
+
+  def to_label
+    "#{name} #{color} #{status}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_085622) do
+ActiveRecord::Schema.define(version: 2020_05_01_080718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_085622) do
   end
 
   create_table "leds", force: :cascade do |t|
-    t.string "icon"
+    t.string "name"
     t.string "color"
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
